c90487ce00f5a66ae89c5d4949c0eb92fb490667
rdeez <gorenzogo@gmail.com>
2017-03-21 01:05:26 -0400

    Made for the home page tiles keep proportions once window height resizes

---
+++ main/js/Image-Resize.js
@@ -0,0 +1,9 @@
+/**
+ * Created by renzodiaz on 3/20/17.
+ */
+$(window).resize(function() {
+    var ratio = 1.6;
+    $('.tile').width($('.tile').height() * ratio);
+    // instead of directly using "$(window).width() * ratio", you can call a method to
+    // calculate width and height each time window is resized.
+})
