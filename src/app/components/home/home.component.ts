3032ed486cdf950b51f372a6c9033869e46c872f
Mauro Doza <state2good@gmail.com>
2017-12-06 21:55:07 -0500

    Addition of custom module and folder. Edit to .gitignore for more precise ignoring of files in node_modules

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -5,32 +5,26 @@ declare var $: any;
 
 @Component({
   selector: "app-home",
-  templateUrl: "./home.component.html",
   styleUrls: ["./home.component.css"],
+  templateUrl: "./home.component.html",
 })
 export class HomeComponent implements OnInit {
-  // posts: Posts[];
-  logo = "./assets/img/logo/diadoz-white-logo-2017.svg";
-  // posts: any = [];
+  public logo = "./assets/img/logo/diadoz-white-logo-2017.svg";
 
-  // for vertical scrolling
   public scrollWindowValue: boolean;
   public horizontalValue: boolean;
 
-  // for horizontal scrolling
-  // scrollWindowValue = false;
-  // horizontalValue = true;
+  public postArray = [];
+  public scrollDistance = 2;
+  public throttle = 300;
+  public pageNumber = 1;
 
-  postArray = [];
-  scrollDistance = 2;
-  throttle = 300;
-  pageNumber = 1;
   constructor(private dataService: DataService ) {
     this.addPosts(this.pageNumber);
   }
 
-  ngOnInit() {
-    let that = this;
+  public ngOnInit() {
+    const that = this;
 
     if (matchMedia) {
       const mql = window.matchMedia("(orientation: landscape)");
@@ -38,16 +32,13 @@ export class HomeComponent implements OnInit {
       orientationChange(mql);
     }
 
-    // media query change
     function orientationChange(m) {
       if (m.matches) {
-        // alert("Changed to landscape");
         that.scrollWindowValue = false;
         that.horizontalValue = true;
         $(".body-container").mousewheel(function(event, delta) {
           event.preventDefault();
 
-          // variable to check if MAC
           const isMac = navigator.platform.toUpperCase().indexOf("MAC") >= 0;
 
           if (isMac) {
@@ -57,55 +48,22 @@ export class HomeComponent implements OnInit {
           }
         });
       } else {
-        // alert("Changed to portrait");
         that.scrollWindowValue = true;
         that.horizontalValue = false;
+
         $(".body-container").unbind();
       }
     }
-
-    // $(document).ready(function($) {
-    //   // Find matches
-      
-
-    //   // Add a media query change listener
-      
-
-    //   $(window).on("deviceorientation", function (event) {
-    //     if (window.matchMedia("(orientation: portrait)").matches) {
-    //       // alert("Device is in portrait mode");
-    //       that.horizontalValue = false;
-    //       $(".body-container").unbind();
-    //     }
-    //     if (window.matchMedia("(orientation: landscape)").matches) {
-    //       // alert("Device is in landscape mode");
-    //       that.horizontalValue = true;
-    //       $(".body-container").mousewheel(function (event, delta) {
-    //         event.preventDefault();
-
-    //         // variable to check if MAC
-    //         const isMac = navigator.platform.toUpperCase().indexOf("MAC") >= 0;
-
-    //         if (isMac) {
-    //           this.scrollLeft -= (delta * .9);
-    //         } else {
-    //           this.scrollLeft -= (delta * 150);
-    //         }
-    //       });
-    //     }
-    //   });
-    // });
   }
 
-  addPosts(page) {
+  public addPosts(page) {
     this.dataService.getPosts(page).subscribe((posts) => {
-      console.log(posts);
       this.postArray = this.postArray.concat(posts);
       this.pageNumber++;
     });
   }
 
-  onScroll() {
+  public onScroll() {
     this.addPosts(this.pageNumber);
   }
 }
